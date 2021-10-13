b8baf06d3b40afd01a9e3fa09d83e6c8323feb48
OuchattiKhadija <ouchattikhadija1@gmail.com>
2021-10-13 15:36:34 +0100

    fix Bug

--- app/src/main/java/net/maribat/retrofitwithrxjava/ui/viewmodel/PostViewModel.java
+++ app/src/main/java/net/maribat/retrofitwithrxjava/ui/viewmodel/PostViewModel.java
@@ -12,8 +12,9 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+
 import io.reactivex.disposables.Disposable;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 public class PostViewModel extends ViewModel {
